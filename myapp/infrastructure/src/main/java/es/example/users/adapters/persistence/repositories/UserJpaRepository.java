5c7161588784cc1098068d535d0f1b1ee836371a
sergioapi <seralpi99@gmail.com>
2025-05-14 19:29:37 +0200

    Limpieza de carpetas
    Eliminar proyectos generados de la carpeta raiz del generador

--- myapp/infrastructure/src/main/java/es/example/users/adapters/persistence/repositories/UserJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package es.example.users.adapters.persistence.repositories;
-
-import es.example.users.adapters.persistence.entities.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
-}
